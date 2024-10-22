a53148fc7b63bf5ef3565246dd5e08af94daed3a
GayathriSanjeevi <139069932+GayathriSanjeevi@users.noreply.github.com>
2024-10-22 19:57:20 +0530

    Update Basic_Math_Calculator.py

--- Python/Basic_Math_Calculator.py
+++ Python/Basic_Math_Calculator.py
@@ -8,7 +8,7 @@ y=st.number_input("Enter the second number:")
 st.write("x is", x)
 st.write("y is", y)
 
-option=st.selectbox("Enter the operation need to be done 1. Addition, 2. Subtraction, 3. Multiplication,  Division",
+option=st.selectbox("Enter the operation need to be done 1. Addition, 2. Subtraction, 3. Multiplication,  4. Division",
                     ("1", "2", "3", "4"))
 
 if option=="1":
